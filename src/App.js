f23d7901bf7fe05edddac1cdd73355b4d0d15b30
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-08-02 12:54:20 +0300

    Remove buggy parallax effect

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Hero from './Hero/Hero.jsx';
 import About from './About/About';
 import MySkills from './MySkills/MySkills';
 import ExperienceNProject from './Experience-And-Project/ExperienceNProject';
-import Education from './Education/Education';
 import * as colors from 'material-ui/colors';
-import BacktoSchool from './Education/static/backtoschool.jpg';
-import ParallaxContainer, { ParallaxGroup } from './components/ParallaxContainer';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
 
@@ -29,32 +26,11 @@ const Container = styled.div`
         padding-bottom: 48px;
     }
 `;
-const Background = styled.div`
-    width: 100%;
-    height: 60vh;
-    background-image: url(${props => props.imgUrl});
-    background-repeat: no-repeat;
-    background-position: left 90%;
-    background-size: cover;
-    filter: blur(1px);
-    transform: translateY(-10%) scale(1.3);
 
-    @media only screen and (max-width: 768px) {
-      background-position: left bottom;
-      height: 80vh;         
-    }
-  }
-`;
-const Parallax = styled(ParallaxGroup)`
-    height: 30vh !important;
-    @media only screen and (max-width: 768px) {
-        height: 480px !important;
-    }
-`;
 class App extends Component {
     render() {
         return (
-            <ParallaxContainer>
+            <div>
                 <Hero />
 
                 <Container backgroundColor={colors.grey[50]}>
@@ -64,13 +40,6 @@ class App extends Component {
                 <Container backgroundColor={colors.grey[50]}>
                     <MySkills />
                 </Container>
-
-                <Parallax backLayer={<Background imgUrl={BacktoSchool} />}>
-                    <Container style={{ height: '100%' }} backgroundColor={'rgba(0,0,0, .6)'}>
-                        <Education />
-                    </Container>
-                </Parallax>
-
                 <Container backgroundColor={colors.grey[900]}>
                     <ExperienceNProject />
                 </Container>
@@ -80,7 +49,7 @@ class App extends Component {
                 </Container>
 
                 <Footer />
-            </ParallaxContainer>
+            </div>
         );
     }
 }
